b9ff2bf77e95f0ef7f4a6135f038ec7e3775440e
ehtom <ehtom@DESKTOP-SS1Q4KR>
2022-05-03 10:53:25 -0400

    Add project files.

---
+++ HF_DelegatesYeet/Adrian.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HF_DelegatesYeet
+{
+    internal class Adrian
+    {
+        public GetSecretIngredient MySecretIngredientMethod
+        {
+            get
+            {
+                return AddAdriansSecretIngredient;
+            }
+        }
+        private string AddAdriansSecretIngredient(int amount) {
+            return $"{amount} ounces of cloves";
+        
+        }
+    }
+}
